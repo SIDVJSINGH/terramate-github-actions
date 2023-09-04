d5afad154d3832612142cf55a9f07bb85c518411
SIDVJSINGH <sidvjsingh@gmail.com>
2023-09-04 21:21:16 +0530

    files seperated

---
+++ S3/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-northeast-1"
+  access_key = "AKIARQSGNPPR24JUC3QG"
+  secret_key = "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"
+}
